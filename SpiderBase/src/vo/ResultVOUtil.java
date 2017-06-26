e90ef8b656fd54a4a598c86cee3ecabc274a3266
胡宏华 <huhh@yonyou.com>
2017-06-26 09:24:52 +0800

    迭代块处理

---
+++ SpiderBase/src/vo/ResultVOUtil.java
@@ -0,0 +1,12 @@
+package vo;
+
+import java.util.Set;
+
+import query.result.ElementBaseResultVO;
+
+public class ResultVOUtil {
+
+	public void translate(ResultBaseVO vo, Set<ElementBaseResultVO> elementVos) {
+		// 动态翻译，获取VO属性、VO属性对应的ElementVO属性、将ElementVO值放入VO
+	}
+}
